9b8e6777ed7fe426b50f3413fb09873e50bf8a78
TheSilvered <taffarello.da@gmail.com>
2024-05-05 20:27:31 +0200

    Aggiunta classe `Token`
    - ora non si può creare un'istanza di `ErroreInterpretazione`

--- src/interpretatore/__init__.py
+++ src/interpretatore/__init__.py
@@ -1,2 +1,2 @@
 from .interpretatore import NodoBase, interpreta_funzione
-from .lexer import ErroreInterpretazione
+from .lexer import ErroreInterpretazioneEsportato as ErroreInterpretazione
--- src/interpretatore/lexer.py
+++ src/interpretatore/lexer.py
@@ -1,9 +1,44 @@
-class ErroreInterpretazione:
-    def __init__(self, message):
-        self.message = message
+from enum import Enum, auto
 
-    def __repr__(self):
-        return self.message
+
+class ErroreInterpretazioneEsportato:
+    def __init__(self, _):
+        raise TypeError("non si può creare un'istanza di ErroreInterpretazione")
 
     def msg(self):
-        return self.message
+        pass
+
+
+class ErroreInterpretazione(ErroreInterpretazioneEsportato):
+    def __init__(self, messaggio):
+        self.messaggio = messaggio
+
+    def __repr__(self):
+        return self.messaggio
+
+    def msg(self):
+        return self.messaggio
+
+
+class TipoToken(Enum):
+    PIU = auto()
+    MENO = auto()
+    NUMERO = auto()
+
+    # per aggiungere un tipo di token scrivere NOME = auto()
+
+
+class Token:
+    # l'attributo valore è utile quando si hanno token ad esempio `NUMERO` con
+    # cui va salvato anche il valore del numero, quando si crea un token si
+    # può non passare un secondo argomento se non si vuole specificare un valore
+    # Token(TipoToken.PIU) -> non specifica un valore
+    # Token(TipoToken.NUMERO, 5.0) -> specifica il valore 5
+    def __init__(self, tipo, valore=None):
+        self.tipo = tipo
+        self.valore = valore
+
+    def __repr__(self):
+        if self.valore is None:
+            return f"Token({self.tipo})"
+        return f"Token({self.tipo}, {self.valore!r})"
