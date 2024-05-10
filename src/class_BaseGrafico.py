3b3845f91e36065f53ce3b7d4540c5299a2b4668
Alessia Ciocan <167518969+caciotta145@users.noreply.github.com>
2024-05-10 10:31:55 +0200

    Update class_BaseGrafico.py

--- src/class_BaseGrafico.py
+++ src/class_BaseGrafico.py
@@ -9,3 +9,24 @@ class BaseGrafico(abc.ABC):
     @abc.abstractmethod
     def disegna(): 
         pass
+#parte 2
+class BaseGrafico(abc.ABC):
+    def __init__(self, tela): 
+        self.tela = tela 
+        self.param = self.crea_param() # io dopo creo un metodo che mi restituisce dei valori che vengono chiamati param 
+    @abc.abstractmethod
+    def crea_param(): 
+        pass
+    @abc.abstractmethod
+    def disegna(): 
+        pass
+class BaseGraficoFunzioneX (BaseGrafico, ABC):
+    def funzione(self, x, parametri):
+    def disegna(): 
+        ................
+class BaseGraficoFunzioneY (BaseGrafico, ABC):
+    def funzione(self, x, parametri):
+    def disegna(): 
+        ................
+    
+
